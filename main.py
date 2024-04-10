fb1fb57f426c4287e2f8067d04ef1c068dcb36f0
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-04-10 14:30:10 +0400

    Add Front Card

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
-window.config(width=800, height=526, padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+front_card = PhotoImage(file="images/card_front.png")
+canvas.create_image(400,263, image=front_card)
+canvas.grid(row=0, column=0, columnspan=2)
 
 window.mainloop()
