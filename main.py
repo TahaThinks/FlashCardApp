684c34abf87d70f1f077027722f60c68753d6bb2
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-04-10 14:46:48 +0400

    Add Buttons

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263, image=front_card)
+canvas.create_image(400, 263, image=front_card)
 canvas.grid(row=0, column=0, columnspan=2)
+
+correct_image = PhotoImage(file="images/right.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+
+# Buttons
+right_button = Button(image=correct_image)
+wrong_button = Button(image=correct_image)
+
 
 window.mainloop()
