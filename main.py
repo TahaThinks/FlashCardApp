b2b237262204e3976255493762fc557fd37b4fb5
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-04-10 14:59:57 +0400

    Extract Data from CSV

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import pandas
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
+
+# Read Data using Pandas
+data = pandas.read_csv("data/french_words.csv")
+data_dic = pandas.DataFrame.to_dict(data)
+print(data_dic)
+
 # UI
 
 window = Tk()
@@ -14,6 +20,10 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 front_card = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=front_card)
 canvas.grid(row=0, column=0, columnspan=2)
+
+# Add Text to the Canvas
+canvas.create_text(400,150, font=LANGUAGE_FONT, text="French")
+canvas.create_text(400,263, font=WORD_FONT, text=data_dic["French"][0])
 
 correct_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
